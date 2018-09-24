45a7c586e60b176ce78dc349527bad05e55d3706
laduelovecalculator <43447806+LadueLoveCalculator@users.noreply.github.com>
2018-09-24 12:35:04 -0500

    updated UI

--- masala.py
+++ masala.py
@@ -2,7 +2,13 @@ import masala_utils
 
 model=masala_utils.recompile_model("weights_utils.hdf5")
 
-n1=raw_input("First Name: ")
-n2=raw_input("Second Name: ")
+print("Ladue Love Calculator.")
+print("To exit, leave both name fields blank.\n")
+
+n1=n2="~"
+
+while n1!="" and n2!="":
+	n1=raw_input("First Name: ")
+	n2=raw_input("Second Name: ")
 
 print(masala_utils.use_model(model, "names.txt", n1, n2))
